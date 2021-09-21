3a9ddaaba228b1b90e6478c1851b7dd1afd6637e
Eden Massalonga <edenmassalonga@outlook.com>
2021-09-21 22:38:33 +0200

    env variables

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from 'react'
 import styled from 'styled-components'
-import { ErrorMessage, useFormik } from 'formik'
+import { useFormik } from 'formik'
 import * as Yup from 'yup';
 import WeatherCard from './sidebar/WeatherCard'
 import { ForecastContext } from '../context/ForecastContext'
